c70e632957beeee908965f17187f78ed484a7dac
Jerel R Hass <agies1@hotmail.com>
2015-08-31 00:53:49 -0400

    Reporting?

--- tests/index.js
+++ tests/index.js
@@ -10,7 +10,14 @@ var board = new five.Board({
 
 board.on("ready", function(){
 	setInterval(function(){
-		exec('sudo sh -c "echo ' + on ? 1 : 0 + ' >/sys/class/leds/led0/brightness"');
+		exec('sudo sh -c "echo ' + (on ? 1 : 0) + ' >/sys/class/leds/led0/brightness"', 
+			function(error, stdout, stderr) {
+				sys.print('stdout: ' + stdout);
+  			sys.print('stderr: ' + stderr);
+  			if (error !== null) {
+    			console.log('exec error: ' + error);
+			  }
+		});
 		on = !on;
 	}, 1000);
 });
