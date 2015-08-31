c76c8d0f66a1cece6982adb7c7adc91a043a2449
Jerel R Hass <agies1@hotmail.com>
2015-08-31 00:50:26 -0400

    trying command line

--- tests/index.js
+++ tests/index.js
@@ -1,11 +1,16 @@
 var five = require("johnny-five"),
 		Raspi = require("raspi-io");
-
+var sys = require('sys')
+var exec = require('child_process').exec;
+var child;
+var on = true;
 var board = new five.Board({
 	io: new Raspi() 
 });
 
 board.on("ready", function(){
-	var led = new five.Led("GPIO16");
-	led.blink();
+	setInterval(function(){
+		exec('sudo sh -c "echo ' + on ? 1 : 0 + ' >/sys/class/leds/led0/brightness"');
+		on = !on;
+	}, 1000);
 });
