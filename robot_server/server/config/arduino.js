1aa037e84af6033fa833196b228d10f0a1692a1f
Jerel R Hass <jerel.hass@gmail.com>
2015-08-31 22:46:40 -0400

    trying to get ardunio and pi happy

--- robot_server/server/config/arduino.js
+++ robot_server/server/config/arduino.js
@@ -7,35 +7,33 @@ var five = require('johnny-five'),
     on = true;
 var sys = require('sys')
 var exec = require('child_process').exec;
-var rBoard = new five.Board({
-	io: new Raspi()
+board = new five.Board({
+  //port: ""
 });
 
-rBoard.on("ready", function(){
-	setInterval(function(){
+board.on('ready', function() {
+  setInterval(function(){
 		exec('sudo sh -c "echo ' + (on ? 1 : 0) + ' >/sys/class/leds/led0/brightness"', 
 			function(error, stdout, stderr) {
-				sys.print('stdout: ' + stdout);
-  			sys.print('stderr: ' + stderr);
   			if (error !== null) {
     			console.log('exec error: ' + error);
 			  }
 		});
 		on = !on;
 	}, 1000);
-});
-
-board = new five.Board({
-  //port: ""
-});
-
-board.on('ready', function() {
   var configs = five.Motor.SHIELD_CONFIGS.ADAFRUIT_V1;
   motors.motor1 = new five.Motor(configs.M1); 
   motors.motor2 = new five.Motor(configs.M2);
   motors.motor3 = new five.Motor(configs.M3);
   motors.motor4 = new five.Motor(configs.M4);
   console.log('board ready, created motors');
+});
+
+var rBoard = new five.Board({
+	io: new Raspi()
+});
+rBoard.on("ready", function(){
+	console.log('raspi board ready, created motors');
 });
 
 module.exports = function(socket) {
