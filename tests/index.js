d726756b3f476d9e679024eb0b81856c16a445d2
Jerel R Hass <agies1@hotmail.com>
2015-09-06 14:02:30 -0400

    updated stop

--- tests/index.js
+++ tests/index.js
@@ -4,30 +4,36 @@ var sys = require('sys')
 var exec = require('child_process').exec;
 var child;
 var on = true;
+var rightServo, leftServo;
+
 var board = new five.Board({
-	io: new Raspi() 
+	io: new Raspi()
 });
 
-board.on("ready", function(){
-	var rightServo = new five.Servo.Continuous("GPIO18");
-	var leftServo = new five.Servo.Continuous("GPIO19");
-	rightServo.ccw(1);
-	leftServo.cw(1);
+board.on("ready", function () {
+	rightServo = new five.Servo.Continuous("GPIO18");
+	leftServo = new five.Servo.Continuous("GPIO19");
+	rightServo.cw(1);
+	leftServo.ccw(1);
 	this.repl.inject({
-	  right: rightServo,
-	  left: leftServo
+		right: rightServo,
+		left: leftServo,
+		stop: stop
 	});
 	signalReady();
 });
-
+function stop() {
+	rightServo.stop();
+	leftServo.stop();
+}
 function signalReady() {
-	setInterval(function(){
-		exec('sudo sh -c "echo ' + (on ? 1 : 0) + ' >/sys/class/leds/led0/brightness"', 
-			function(error, stdout, stderr) {
-  			if (error !== null) {
-    			console.log('exec error: ' + error);
-			  }
-		});
+	setInterval(function () {
+		exec('sudo sh -c "echo ' + (on ? 1 : 0) + ' >/sys/class/leds/led0/brightness"',
+			function (error, stdout, stderr) {
+				if (error !== null) {
+					console.log('exec error: ' + error);
+				}
+			});
 		on = !on;
 	}, 1000);
 }
